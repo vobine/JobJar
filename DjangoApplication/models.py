1b2322ccd1033879f6fae5d3622fabf9691c2c36
Hal Peterson <vobine@users.noreply.github.com>
2015-03-28 08:22:11 -0500

    Add ownership to objects

--- DjangoApplication/models.py
+++ DjangoApplication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Implementation parameters.
 CHAR_LIMITS = {
@@ -8,6 +9,11 @@ CHAR_LIMITS = {
 }
 
 # Create your models here.
+
+# We subclass User to turn it into something more flexible someday.
+class JobOwner (User):
+    """This will include some more elaborate permission mechanism."""
+    pass
 
 # All models have a default ID field.
 
@@ -21,6 +27,7 @@ class Job (models.Model):
     """An individual task to be scheduled. Grist for the mill."""
     name = models.CharField (max_length=CHAR_LIMITS['name'])
     desc = models.CharField (max_length=CHAR_LIMITS['desc'])
+    owner = models.ForeignKey (JobOwner, null=True)
     state = models.ForeignKey (JobState)
 
 class JobEventType (models.Model):
@@ -32,6 +39,7 @@ class JobEventType (models.Model):
 class JobLog (models.Model):
     """A state change for a Job."""
     job = models.ForeignKey (Job)
+    owner = models.ForeignKey (JobOwner, null=True)
     event = models.ForeignKey (JobEventType)
     timestamp = models.DateTimeField (auto_now_add=True)
     note = models.CharField (max_length=CHAR_LIMITS['desc'])
