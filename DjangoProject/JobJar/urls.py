ba99413cc12f85b0904d336c78583e84aeb1dda3
Hal Peterson <vobine@users.noreply.github.com>
2015-04-05 16:26:25 -0500

    Minimal root page, with URL linkage, including namespacing

--- DjangoProject/JobJar/urls.py
+++ DjangoProject/JobJar/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'JobJar.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url ('^JobJar/',
+                            include ('JobJarApplication.urls',
+                                     namespace='jobjar')),
+                       url(r'^admin/', 
+                           include(admin.site.urls)),
 )
