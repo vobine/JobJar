029ce2b9926523642568acb1ff1da537d36281ea
Hal Peterson <vobine@users.noreply.github.com>
2015-05-24 19:18:29 -0500

    Migration from model update

---
+++ JobJarProject/JobJarApplication/migrations/0003_auto_20150525_0003.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('JobJarApplication', '0002_auto_20150328_1320'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='job',
+            name='state',
+            field=models.IntegerField(choices=[(1, b'Uninitialized'), (2, b'Busy'), (3, b'Available'), (4, b'Idle'), (5, b'Completed'), (6, b'Unknown')]),
+            preserve_default=True,
+        ),
+        migrations.DeleteModel(
+            name='JobState',
+        ),
+    ]
